7c09a1bd595b427842a976ba91e876fe41dda867
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-03-04 15:14:18 +0300

    Added a turn_page method to the book class

--- lib/book.py
+++ lib/book.py
@@ -16,4 +16,6 @@ class Book:
         else:
             print("page_count must be an integer.")
         
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
